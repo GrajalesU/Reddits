5ec9e3446118335c491e49bf87e82b060a9b960e
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-27 13:21:42 -0500

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SubredditDetail from "./components/SubredditDetail";
 import SubredditList from "./components/SubredditList";
 
 function App() {
   return (
-    <div className="container">
-      <hgroup>
-        <h1> PRUEBA TÉCNICA:</h1>
-        <h2>r/Juan Manuel Grajales</h2>
-      </hgroup>
-      <SubredditList />
-      <SubredditDetail />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <hgroup>
+          <h1> PRUEBA TÉCNICA:</h1>
+          <h2>r/Juan Manuel Grajales</h2>
+        </hgroup>
+        <Routes>
+          <Route exact path="/" element={<SubredditList />} />
+          <Route path="/subreddit/:id" element={<SubredditDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
