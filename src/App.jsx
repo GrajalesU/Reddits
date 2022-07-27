f60db1ccf28bb0ea37f6e70ba3a0da54c09a7e2a
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-07-27 11:30:55 -0500

    Add subreddit list component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import SubredditList from "./components/SubredditList";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <hgroup>
+        <h1> PRUEBA TÉCNICA:</h1>
+        <h2>r/Juan Manuel Grajales</h2>
+      </hgroup>
+      <SubredditList />
     </div>
   );
 }
